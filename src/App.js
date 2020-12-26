f6ab3ed3b7115480db62896533fd4a07e478239c
O Y <osyousufi@gmail.com>
2020-12-26 11:48:37 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const items = [
+
+  {
+    title: 'React is so Cool',
+    content: 'This is content that is being displayed here but original'
+  },
+  {
+    title: 'React is Helpful',
+    content: 'This is content that is being displayed here but cool'
+  },
+  {
+    title: 'Wow I Like React!',
+    content: 'This is content that is being displayed here but cooler'
+  }
+
+]
+
+const options = [
+  {
+    label: 'The color red',
+    value: 'red'
+  },
+  {
+    label: 'The color green',
+    value: 'green'
+  },
+  {
+    label: 'The color blue',
+    value: 'blue'
+  }
+
+]
+
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return(
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <Dropdown label="Select a color:" options={options} selected={selected} onSelectedChange={setSelected}/>
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
+
 }
 
 export default App;
